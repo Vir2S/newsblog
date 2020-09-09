c9b58b8712a86af3ca215a4e34a12e53b268830a
Vitaly Sem <virtualik@gmail.com>
2020-09-09 20:01:08 +0300

    admin Post registered

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Vitaly Sem',
-        'title': 'Blog post 1',
-        'content': 'Post 1 content',
-        'date_posted': 'September 09, 2020',
-    },
-    {
-        'author': 'Vitaly Sem',
-        'title': 'Blog post 2',
-        'content': 'Post 2 content',
-        'date_posted': 'September 09, 2020',
-    }
-]
+from .models import Post
 
 
 def home(request):
 
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
 
     return render(request, 'blogapp/home.html', context=context)
