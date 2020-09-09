2a439c296f0de03b2e13243cbc7ee77a43e3c5d8
Vitaly Sem <virtualik@gmail.com>
2020-09-09 16:30:27 +0300

    templates added

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Vitaly Sem',
+        'title': 'Blog post 1',
+        'content': 'Post 1 content',
+        'date_posted': 'September 09, 2020',
+    },
+    {
+        'author': 'Vitaly Sem',
+        'title': 'Blog post 2',
+        'content': 'Post 2 content',
+        'date_posted': 'September 09, 2020',
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blogapp/home.html', context=context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blogapp/about.html', context={'title': 'About'})
