f24bdb9f0f418418256d74fddf9d49b15ee0f0b4
Vitaly Sem <virtualik@gmail.com>
2020-09-10 13:00:23 +0300

    users view modified

--- blog/users/views.py
+++ blog/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
+from .forms import UserRegisterForm
 
 
 def register(request):
@@ -12,11 +15,16 @@ def register(request):
 
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account for {username} created')
+            messages.success(request, f'Account for {username} created. You are now able to log in.')
 
-            return redirect('blog-home')
+            return redirect('login')
 
-    else:
-        form = UserCreationForm()
+        else:
+            form = UserRegisterForm()
 
-    return render(request, 'users/register.html', {'form': form})
+        return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
