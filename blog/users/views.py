b66f813964176708c60e59f9ecc95d5257f61b5f
Vitaly Sem <virtualik@gmail.com>
2020-09-10 13:41:36 +0300

    user views fixed

--- blog/users/views.py
+++ blog/users/views.py
@@ -17,12 +17,12 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account for {username} created. You are now able to log in.')
 
-            return redirect('login')
+            return redirect('login/')
 
-        else:
-            form = UserRegisterForm()
+    else:
+        form = UserRegisterForm()
 
-        return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
