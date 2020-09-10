ea93bac5dee09484bf2d3ca17866749bafbc3340
Vitaly Sem <virtualik@gmail.com>
2020-09-10 22:26:37 +0300

    blogapp models fixed

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -7,9 +7,9 @@ class Post(models.Model):
 
     title = models.CharField(max_length=150)
     content = models.TextField()
-    date_created = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     date_posted = models.DateTimeField(default=timezone.now)
-    date_updated = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
