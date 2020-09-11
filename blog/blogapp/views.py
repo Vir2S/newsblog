8ad88277be949f9fa773d6920a78464239fddecf
Vitaly Sem <virtualik@gmail.com>
2020-09-11 15:57:29 +0300

    blogapp views updated

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -27,10 +27,22 @@ class PostListView(ListView):
     template_name = 'blogapp/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 10
+
+
+class UserPostListView(ListView):
+
+    model = Post
+    template_name = 'blogapp/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
-
     model = Post
 
 
@@ -40,9 +52,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     fields = ['title', 'content']
 
     def form_valid(self, form):
-
         form.instance.author = self.request.user
-
         return super().form_valid(form)
 
 
@@ -52,17 +62,13 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     fields = ['title', 'content']
 
     def form_valid(self, form):
-
         form.instance.author = self.request.user
-
         return super().form_valid(form)
 
     def test_func(self):
-
         post = self.get_object()
 
         if self.request.user == post.author:
-
             return True
 
         return False
@@ -78,7 +84,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         post = self.get_object()
 
         if self.request.user == post.author:
-
             return True
 
         return False
