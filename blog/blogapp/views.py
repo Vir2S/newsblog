caee2eb945315ab06405381ae163303680ce8a1a
Vitaly Sem <virtualik@gmail.com>
2020-09-11 20:34:45 +0300

    post views and post_detail template fixed

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -49,7 +49,7 @@ class PostDetailView(DetailView):
 class PostCreateView(LoginRequiredMixin, CreateView):
 
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'post_image', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
