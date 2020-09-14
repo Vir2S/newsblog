8fc9e57a1524d70fc15d0ae120dbfcb9915a3f57
Vitaly Sem <virtualik@gmail.com>
2020-09-14 21:07:18 +0300

    roll back users models

--- blog/users/models.py
+++ blog/users/models.py
@@ -22,14 +22,3 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
