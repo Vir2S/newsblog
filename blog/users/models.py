2352f19eff7535973ed219e77e5d32b2246385f7
Vitaly Sem <virtualik@gmail.com>
2020-09-14 22:20:24 +0300

    profile creation fixed

--- blog/users/models.py
+++ blog/users/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from django.dispatch import receiver
 
 from PIL import Image
 
@@ -13,8 +11,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
@@ -22,3 +20,6 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+import users.signals
